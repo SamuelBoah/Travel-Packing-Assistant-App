0ab135cf15122df03c1d54802fb59c25daa76677
SamuelBoah <samuelboah004@gmail.com>
2025-07-25 00:29:22 -0700

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import ParkingList from "./ParkingList";
+import Stats from "./Stats";
+
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "Clothes", quantity: 10, packed: true },
+// ];
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((prevItems) => [...prevItems, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to clear everything?"
+    ); // another type of alert but it comes with 'OK' and 'Cancel' .. unlike alert that comes with only 'Ok'
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <ParkingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
+
+export default App;
